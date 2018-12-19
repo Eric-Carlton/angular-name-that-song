f3587b42fe747fbac07b8e611a3103206899aa77
Eric-Carlton <eric121492@gmail.com>
2018-12-18 20:25:34 -0500

    rename name-that-song-angular -> angular-name-that-song

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to name-that-song-angular!');
+    expect(page.getTitleText()).toEqual('Welcome to angular-name-that-song!');
   });
 });
